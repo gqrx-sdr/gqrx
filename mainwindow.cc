66578d2b7fe542c4844dd751c2078b30f198fa89
Alexandru Csete <oz9aec@gmail.com>
2011-05-31 00:17:24 +0200

    Added label to display receiving frequency.

--- mainwindow.cc
+++ mainwindow.cc
@@ -30,6 +30,7 @@ MainWindow::MainWindow(QWidget *parent) :
     /* frequency control widget */
     ui->freqCtrl->Setup(10, (quint64) 50e6, (quint64) 2e9, 1, UNITS_MHZ);
     ui->freqCtrl->SetFrequency(144500000);
+    ui->rxFreqLabel->setText("144.500000 MHz");
 
     /* create receiver object */
     rx = new receiver("hw:1");
@@ -55,7 +56,12 @@ MainWindow::~MainWindow()
  */
 void MainWindow::setNewFrequency(qint64 freq)
 {
+    double rx_freq_mhz;
+
     rx->set_rf_freq((float) freq);
+
+    rx_freq_mhz = (freq + rx->get_filter_offset()) / 1.0e6;
+    ui->rxFreqLabel->setText(QString("%1 MHz").arg(rx_freq_mhz, 11, 'f', 6, ' '));
 }
 
 
@@ -76,8 +82,14 @@ void MainWindow::on_rxStartStopButton_toggled(bool checked)
 /* temporary slot for receiving slider signals (tune within passband) */
 void MainWindow::on_tuningSlider_valueChanged(int value)
 {
-    qDebug() << "TUNE: " << value;
+    double rx_freq_mhz;
+
+    //qDebug() << "TUNE: " << value;
     rx->set_filter_offset((double) value);
+
+    rx_freq_mhz = (value + rx->get_rf_freq()) / 1.0e6;
+    ui->rxFreqLabel->setText(QString("%1 MHz").arg(rx_freq_mhz, 11, 'f', 6, ' '));
+
 }
 
 
