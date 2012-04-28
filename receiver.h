feb0c74469fe41f9d30640226bb10e618c3ee2ef
Alexandru Csete <oz9aec@gmail.com>
2012-04-28 15:23:49 +0200

    Fix doxygen tags.

--- receiver.h
+++ receiver.h
@@ -64,24 +64,24 @@ public:
 
     /*! \brief Flag used to indicate success or failure of an operation, usually a set_something().     */
     enum status {
-        STATUS_OK    = 0, /*! Operation was successful. */
-        STATUS_ERROR = 1  /*! There was an error. */
+        STATUS_OK    = 0, /*!< Operation was successful. */
+        STATUS_ERROR = 1  /*!< There was an error. */
     };
 
     /*! \brief Available demodulators. */
     enum demod {
-        DEMOD_NONE = 0,  /*! No demod. Raw I/Q to audio. */
-        DEMOD_AM   = 1,  /*! Amplitude modulation. */
-        DEMOD_FM   = 2,  /*! Frequency modulation. */
-        DEMOD_SSB  = 3,  /*! Single Side Band. */
-        DEMOD_NUM  = 4   /*! Included for convenience. */
+        DEMOD_NONE = 0,  /*!< No demod. Raw I/Q to audio. */
+        DEMOD_AM   = 1,  /*!< Amplitude modulation. */
+        DEMOD_FM   = 2,  /*!< Frequency modulation. */
+        DEMOD_SSB  = 3,  /*!< Single Side Band. */
+        DEMOD_NUM  = 4   /*!< Included for convenience. */
     };
 
     /*! \brief Filter shape (convenience wrappers for "transition width"). */
     enum filter_shape {
-        FILTER_SHAPE_SOFT = 0,   /*! Soft: Transition band is TBD of width. */
-        FILTER_SHAPE_NORMAL = 1, /*! Normal: Transition band is TBD of width. */
-        FILTER_SHAPE_SHARP = 2   /*! Sharp: Transition band is TBD of width. */
+        FILTER_SHAPE_SOFT = 0,   /*!< Soft: Transition band is TBD of width. */
+        FILTER_SHAPE_NORMAL = 1, /*!< Normal: Transition band is TBD of width. */
+        FILTER_SHAPE_SHARP = 2   /*!< Sharp: Transition band is TBD of width. */
     };
 
 
@@ -157,44 +157,44 @@ public:
     void   get_sniffer_data(float * outbuff, int &num);
 
 private:
-    bool   d_running;          /*! Whether receiver is running or not. */
-    float  d_bandwidth;        /*! Receiver bandwidth. */
-    int    d_audio_rate;       /*! Audio output rate. */
-    double d_rf_freq;          /*! Current RF frequency. */
-    double d_filter_offset;    /*! Current filter offset (tune within passband). */
-    bool   d_recording_iq;     /*! Whether we are recording I/Q data. */
-    bool   d_recording_wav;    /*! Whether we are recording WAV file. */
-    bool   d_sniffer_active;   /*! Only one data decoder allowed. */
+    bool   d_running;          /*!< Whether receiver is running or not. */
+    float  d_bandwidth;        /*!< Receiver bandwidth. */
+    int    d_audio_rate;       /*!< Audio output rate. */
+    double d_rf_freq;          /*!< Current RF frequency. */
+    double d_filter_offset;    /*!< Current filter offset (tune within passband). */
+    bool   d_recording_iq;     /*!< Whether we are recording I/Q data. */
+    bool   d_recording_wav;    /*!< Whether we are recording WAV file. */
+    bool   d_sniffer_active;   /*!< Only one data decoder allowed. */
 
-    demod  d_demod;          /*! Current demodulator. */
+    demod  d_demod;          /*!< Current demodulator. */
 
-    gr_top_block_sptr         tb;        /*! The GNU Radio top block. */
+    gr_top_block_sptr         tb;        /*!< The GNU Radio top block. */
 
-    rx_source_base_sptr       src;       /*! Real time I/Q source. */
+    rx_source_base_sptr       src;       /*!< Real time I/Q source. */
 
-    dc_corr_cc_sptr           dc_corr;   /*! DC corrector block. */
+    dc_corr_cc_sptr           dc_corr;   /*!< DC corrector block. */
 
-    rx_fft_c_sptr             fft;       /*! Receiver FFT block. */
-    rx_filter_sptr            filter;
-    rx_meter_c_sptr           meter;      /*! Signal strength. */
-    rx_agc_cc_sptr            agc;        /*! Receiver AGC. */
-    gr_simple_squelch_cc_sptr sql;        /*! Squelch. */
-    gr_complex_to_real_sptr   demod_ssb;  /*! SSB demodulator. */
-    rx_demod_fm_sptr          demod_fm;   /*! FM demodulator. */
-    rx_demod_am_sptr          demod_am;   /*! AM demodulator. */
-    resampler_ff_sptr         audio_rr;   /*! Audio resampler. */
-    gr_multiply_const_ff_sptr audio_gain; /*! Audio gain block. */
+    rx_fft_c_sptr             fft;       /*!< Receiver FFT block. */
+    rx_filter_sptr            filter;     /*!< BAndpass filter. */
+    rx_meter_c_sptr           meter;      /*!< Signal strength. */
+    rx_agc_cc_sptr            agc;        /*!< Receiver AGC. */
+    gr_simple_squelch_cc_sptr sql;        /*!< Squelch. */
+    gr_complex_to_real_sptr   demod_ssb;  /*!< SSB demodulator. */
+    rx_demod_fm_sptr          demod_fm;   /*!< FM demodulator. */
+    rx_demod_am_sptr          demod_am;   /*!< AM demodulator. */
+    resampler_ff_sptr         audio_rr;   /*!< Audio resampler. */
+    gr_multiply_const_ff_sptr audio_gain; /*!< Audio gain block. */
 
-    gr_file_sink_sptr         iq_sink;    /*! I/Q file sink. */
-    gr_file_source_sptr       iq_src;     /*! I/Q file source. */
-    gr_throttle::sptr         iq_throttle; /*! Throttle for I/Q playback (in case we don't use audio sink) */
-    gr_wavfile_sink_sptr      wav_sink;   /*! WAV file sink for recording. */
-    gr_wavfile_source_sptr    wav_src;    /*! WAV file source for playback. */
-    gr_null_sink_sptr         audio_null_sink; /*! Audio null sink used during playback. */
-    sniffer_f_sptr            sniffer;    /*! Sample sniffer for data decoders. */
-    resampler_ff_sptr         sniffer_rr; /*! Sniffer resampler. */
+    gr_file_sink_sptr         iq_sink;    /*!< I/Q file sink. */
+    gr_file_source_sptr       iq_src;     /*!< I/Q file source. */
+    gr_throttle::sptr         iq_throttle; /*!< Throttle for I/Q playback (in case we don't use audio sink) */
+    gr_wavfile_sink_sptr      wav_sink;   /*!< WAV file sink for recording. */
+    gr_wavfile_source_sptr    wav_src;    /*!< WAV file source for playback. */
+    gr_null_sink_sptr         audio_null_sink; /*!< Audio null sink used during playback. */
+    sniffer_f_sptr            sniffer;    /*!< Sample sniffer for data decoders. */
+    resampler_ff_sptr         sniffer_rr; /*!< Sniffer resampler. */
 
-    pa_sink_sptr              audio_snk;  /*! Audio sink. */
+    pa_sink_sptr              audio_snk;  /*!< Audio sink. */
 
 protected:
 
