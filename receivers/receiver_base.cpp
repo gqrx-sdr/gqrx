bb3d70757a1fa03cd0b2587530072e49e9f44cd8
Alexandru Csete <oz9aec@gmail.com>
2012-05-12 21:31:45 +0200

    Base class for receivers.

---
+++ receivers/receiver_base.cpp
@@ -0,0 +1,40 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2012 Alexandru Csete OZ9AEC.
+ *
+ * Gqrx is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * Gqrx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gqrx; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+#include <gr_io_signature.h>
+#include "receivers/receiver_base.h"
+
+
+static const int MIN_IN = 1;  /* Mininum number of input streams. */
+static const int MAX_IN = 1;  /* Maximum number of input streams. */
+static const int MIN_OUT = 1; /* Minimum number of output streams. */
+static const int MAX_OUT = 2; /* Maximum number of output streams. */
+
+receiver_base_cf::receiver_base_cf(std::string src_name)
+    : gr_hier_block2 (src_name,
+                      gr_make_io_signature (MIN_IN, MAX_IN, sizeof(gr_complex)),
+                      gr_make_io_signature (MIN_OUT, MAX_OUT, sizeof(float)))
+{
+
+}
+
+receiver_base_cf::~receiver_base_cf()
+{
+
+}
---
+++ receivers/receiver_base.h
@@ -0,0 +1,85 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2012 Alexandru Csete OZ9AEC.
+ *
+ * Gqrx is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * Gqrx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Gqrx; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+#ifndef RECEIVER_BASE_H
+#define RECEIVER_BASE_H
+
+#include <gr_hier_block2.h>
+
+
+class receiver_base_cf;
+
+typedef boost::shared_ptr<receiver_base_cf> receiver_base_cf_sptr;
+
+
+/*! \brief Base class for receivers that output audio.
+ *  \ingroup RX
+ *
+ * This block provides a base class and common interface for receivers that
+ * outpout audio (or other kind of float data).
+ *
+ */
+class receiver_base_cf : public gr_hier_block2
+{
+
+public:
+    /*! \brief Public contructor.
+     *  \param src_name Descriptive name used in the contructor of gr_hier_block2
+     */
+    receiver_base_cf(std::string src_name);
+    ~receiver_base_cf();
+
+    virtual bool start() = 0;
+    virtual bool stop() = 0;
+
+    virtual void set_quad_rate(float quad_rate) = 0;
+    virtual void set_audio_rate(float audio_rate) = 0;
+
+    virtual void set_filter(double low, double high, double tw) = 0;
+    virtual void set_filter_low(double freq_hz) = 0;
+    virtual void set_filter_high(double freq_hz) = 0;
+    virtual void set_filter_trans(double tw) = 0;
+
+    virtual float get_signal_level() = 0;
+
+    /* Noise blanker */
+    virtual bool has_nb() = 0;
+    virtual void set_nb_on(int nbid, bool on) = 0;
+    virtual void set_nb_threshold(int nbid, float threshold) = 0;
+
+    /* Squelch parameter */
+    virtual bool has_sql() = 0;
+    virtual void set_sql_level(double level_db) = 0;
+    virtual void set_sql_alpha(double alpha) = 0;
+
+    /* AGC */
+    virtual bool has_agc() = 0;
+    virtual void set_agc_on(bool agc_on) = 0;
+    virtual void set_agc_hang(bool use_hang) = 0;
+    virtual void set_agc_threshold(int threshold) = 0;
+    virtual void set_agc_slope(int slope) = 0;
+    virtual void set_agc_decay(int decay_ms) = 0;
+    virtual void set_agc_manual_gain(int gain) = 0;
+
+    virtual void set_demod(int demod) = 0;
+
+    /* demod specific methods are not part of interface */
+};
+
+#endif // RECEIVER_BASE_H
