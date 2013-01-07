3af62c1b1d0abb8d88192e82124ac73ae80c6ad2
Alexandru Csete <oz9aec@gmail.com>
2013-01-07 23:14:47 +0100

    Backend for AM DCR on/off switching.

--- dsp/rx_demod_am.cpp
+++ dsp/rx_demod_am.cpp
@@ -49,22 +49,20 @@ rx_demod_am::rx_demod_am(float quad_rate, float audio_rate, bool dcr)
     connect(self(), 0, d_demod, 0);
 
     /* DC removal */
+    d_fftaps.resize(2);
+    d_fbtaps.resize(2);
+    d_fftaps[0] = 1.0;      // FIXME: could be configurable with a specified time constant
+    d_fftaps[1] = -1.0;
+    d_fbtaps[0] = 0.0;
+    d_fbtaps[1] = 0.999;
+    d_dcr = gr_make_iir_filter_ffd(d_fftaps, d_fbtaps);
+
     if (d_dcr_enabled) {
-        d_fftaps.resize(2);
-        d_fbtaps.resize(2);
-
-        d_fftaps[0] = 1.0;      // FIXME: could be configurable with a specified time constant
-        d_fftaps[1] = -1.0;
-        d_fbtaps[0] = 0.0;
-        d_fbtaps[1] = 0.999;
-
-        d_dcr = gr_make_iir_filter_ffd(d_fftaps, d_fbtaps);
-
         connect(d_demod, 0, d_dcr, 0);
-        connect(d_dcr, 0,  self(), 0);
+        connect(d_dcr, 0, self(), 0);
     }
     else {
-        connect(d_demod, 0,  self(), 0);
+        connect(d_demod, 0, self(), 0);
     }
 
 }
@@ -81,6 +79,25 @@ void rx_demod_am::set_dcr(bool dcr)
 {
     if (dcr == d_dcr_enabled) {
         return;
+    }
+
+    if (d_dcr_enabled)
+    {
+        // Switching from ON to OFF
+        lock();
+        disconnect(d_demod, 0, d_dcr, 0);
+        disconnect(d_dcr, 0, self(), 0);
+        connect(d_demod, 0, self(), 0);
+        unlock();
+    }
+    else
+    {
+        // Switching from OFF to ON
+        lock();
+        disconnect(d_demod, 0, self(), 0);
+        connect(d_demod, 0, d_dcr, 0);
+        connect(d_dcr, 0, self(), 0);
+        unlock();
     }
 
     d_dcr_enabled = dcr;
