gqrx-sdr/gqrx

commit 58b527320cabe19c15d84373e750747681dd29ce

Change frequency display concept.
Due to user demand the main frequency control and display widget now displays
the receiver frequency and not the hardware frequency. Changes to the shown
frequewncy will still lead to hardware tuning though, but now taking the filter
offset into account.

applications/gqrx/mainwindow.cpp
@@ -39,6 +39,7 @@ MainWindow::MainWindow(const QString cfgfile, QWidget *parent) :
     configOk(true),
     ui(new Ui::MainWindow),
     d_lnb_lo(0),
+    d_hw_freq(0),
     d_fftAvg(0.5),
     d_have_audio(true),
     dec_afsk1200(0)
@@ -423,18 +424,19 @@ void MainWindow::updateFrequencyRange(bool ignore_limits)
  *  \param[in] freq The new frequency.
  *
  * This slot is connected to the CFreqCtrl::newFrequency() signal and is used
- * to set new RF frequency.
+ * to set new receive frequency.
  */
-void MainWindow::setNewFrequency(qint64 freq)
+void MainWindow::setNewFrequency(qint64 rx_freq)
 {
-    /* set receiver frequency */
-    rx->set_rf_freq((double) (freq-d_lnb_lo));
+    double hw_freq = (double)(rx_freq-d_lnb_lo) - rx->get_filter_offset();
+    d_hw_freq = (qint64)hw_freq;
 
-    /* update pandapter */
-    ui->plotter->setCenterFreq(freq);
+    // set receiver frequency
+    rx->set_rf_freq(hw_freq);
 
-    /* update RX frequncy label in rxopts */
-    uiDockRxOpt->setRfFreq(freq);
+    // update widgets
+    ui->plotter->setCenterFreq(d_hw_freq);
+    uiDockRxOpt->setHwFreq(d_hw_freq);
 }
 
 /*! \brief Set new LNB LO frequency.
@@ -454,12 +456,15 @@ void MainWindow::setLnbLo(double freq_mhz)
 }
 
 /*! \brief Set new channel filter offset.
- *  \param freq_hs The new filter offset in Hz.
+ *  \param freq_hz The new filter offset in Hz.
  */
 void MainWindow::setFilterOffset(qint64 freq_hz)
 {
     rx->set_filter_offset((double) freq_hz);
     ui->plotter->setFilterOffset(freq_hz);
+
+    qint64 rx_freq = d_hw_freq + d_lnb_lo + freq_hz;
+    ui->freqCtrl->setFrequency(rx_freq);
 }
 
 /*! \brief Set RF gain.
@@ -1385,7 +1390,7 @@ void MainWindow::on_plotter_newDemodFreq(qint64 freq, qint64 delta)
 
     // update RF freq label and channel filter offset
     uiDockRxOpt->setFilterOffset(delta);
-    uiDockRxOpt->setRfFreq(freq-delta);
+    ui->freqCtrl->setFrequency(freq);
 }
 
 /* CPlotter::NewfilterFreq() is emitted */

applications/gqrx/mainwindow.h
@@ -58,7 +58,7 @@ signals:
     void configChanged(QSettings *settings); /*!< New configuration has been loaded. */
 
 public slots:
-    void setNewFrequency(qint64 freq);
+    void setNewFrequency(qint64 rx_freq);
 
 private:
     Ui::MainWindow *ui;
@@ -68,6 +68,7 @@ private:
     QString             m_last_dir;
 
     qint64 d_lnb_lo;  /* LNB LO in Hz. */
+    qint64 d_hw_freq;
 
     enum receiver::filter_shape d_filter_shape;
     std::complex<float>* d_fftData;

applications/gqrx/mainwindow.ui
@@ -56,6 +56,9 @@
           <height>41</height>
          </size>
         </property>
+        <property name="toolTip">
+         <string/>
+        </property>
         <property name="frameShape">
          <enum>QFrame::StyledPanel</enum>
         </property>

qtgui/dockrxopt.cpp
@@ -28,7 +28,7 @@ DockRxOpt::DockRxOpt(qint64 filterOffsetRange, QWidget *parent) :
     QDockWidget(parent),
     ui(new Ui::DockRxOpt),
     agc_is_on(true),
-    rf_freq_hz(144500000)
+    hw_freq_hz(144500000)
 {
     ui->setupUi(this);
 
@@ -77,7 +77,6 @@ DockRxOpt::~DockRxOpt()
 void DockRxOpt::setFilterOffset(qint64 freq_hz)
 {
     ui->filterFreq->setFrequency(freq_hz);
-    updateRxFreq();
 }
 
 /*! \brief Set filter offset range.
@@ -96,18 +95,18 @@ void DockRxOpt::setFilterOffsetRange(qint64 range_hz)
  * The actual RX frequency is the sum of the RF frequency and the filter
  * offset.
  */
-void DockRxOpt::setRfFreq(qint64 freq_hz)
+void DockRxOpt::setHwFreq(qint64 freq_hz)
 {
-    rf_freq_hz = freq_hz;
-    updateRxFreq();
+    hw_freq_hz = freq_hz;
+    updateHwFreq();
 }
 
 
 /*! \brief Update RX frequency label. */
-void DockRxOpt::updateRxFreq()
+void DockRxOpt::updateHwFreq()
 {
-    double rx_freq_mhz = (rf_freq_hz + ui->filterFreq->getFrequency()) / 1.0e6;
-    ui->rxFreq->setText(QString("%1 MHz").arg(rx_freq_mhz, 11, 'f', 6, ' '));
+    double hw_freq_mhz = hw_freq_hz / 1.0e6;
+    ui->hwFreq->setText(QString("%1 MHz").arg(hw_freq_mhz, 11, 'f', 6, ' '));
 }
 
 
@@ -223,7 +222,7 @@ void DockRxOpt::saveSettings(QSettings *settings)
 void DockRxOpt::on_filterFreq_newFrequency(qint64 freq)
 {
     qDebug() << "New filter offset:" << freq << "Hz";
-    updateRxFreq();
+    updateHwFreq();
 
     emit filterOffsetChanged(freq);
 }

qtgui/dockrxopt.h
@@ -75,7 +75,7 @@ public:
     void setCurrentFilter(int index);
     int  currentFilter();
 
-    void setRfFreq(qint64 freq_hz);
+    void setHwFreq(qint64 freq_hz);
 
     void setCurrentDemod(int demod);
     int  currentDemod();
@@ -83,7 +83,7 @@ public:
     float currentMaxdev();
 
 private:
-    void updateRxFreq();
+    void updateHwFreq();
 
 signals:
     /*! \brief Signal emitted when the channel filter frequency has changed. */
@@ -165,7 +165,7 @@ private:
 
     bool agc_is_on;
 
-    qint64 rf_freq_hz;   /*! Current RF frequency in Hz. Used to display RX frequency. */
+    qint64 hw_freq_hz;   /*! Current PLL frequency in Hz. */
 };
 
 #endif // DOCKRXOPT_H

qtgui/dockrxopt.ui
@@ -74,7 +74,7 @@ This is an offset from the hardware RF frequency.&lt;/p&gt;&lt;/body&gt;&lt;/htm
 as  rf_freq + filter_offset</string>
         </property>
         <property name="text">
-         <string>Receive:</string>
+         <string>Hardware freq:</string>
         </property>
         <property name="alignment">
          <set>Qt::AlignLeading|Qt::AlignLeft|Qt::AlignVCenter</set>
@@ -82,10 +82,9 @@ as  rf_freq + filter_offset</string>
        </widget>
       </item>
       <item>
-       <widget class="QLabel" name="rxFreq">
+       <widget class="QLabel" name="hwFreq">
         <property name="toolTip">
-         <string>The frequency we actualy are listening on.
-Calculated as  rf_freq + filter_offset</string>
+         <string>The frequency of the hardware PLL</string>
         </property>
         <property name="text">
          <string>144.500000 MHz</string>

commit b336e1c4777176eb215801d406acefd44449c359

Fix incorrect tuning when using LNB LO.
Fixes the wrong behaviour introduced by commit 58b527320cabe19c15d84373e750747681dd29ce
and later.

applications/gqrx/mainwindow.cpp
@@ -429,13 +429,15 @@ void MainWindow::updateFrequencyRange(bool ignore_limits)
 void MainWindow::setNewFrequency(qint64 rx_freq)
 {
     double hw_freq = (double)(rx_freq-d_lnb_lo) - rx->get_filter_offset();
+    qint64 center_freq = rx_freq - (qint64)rx->get_filter_offset();
+
     d_hw_freq = (qint64)hw_freq;
 
     // set receiver frequency
     rx->set_rf_freq(hw_freq);
 
     // update widgets
-    ui->plotter->setCenterFreq(d_hw_freq);
+    ui->plotter->setCenterFreq(center_freq);
     uiDockRxOpt->setHwFreq(d_hw_freq);
 }
 