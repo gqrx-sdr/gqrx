17dda9221e5d21bdbba23dca12598bcf3424c216
Alexandru Csete <oz9aec@gmail.com>
2014-08-04 23:54:39 +0200

    Implement user selectable I/Q file location.
    The user can now select where to store raw I/Q files ($HOME by default).

--- applications/gqrx/mainwindow.cpp
+++ applications/gqrx/mainwindow.cpp
@@ -206,7 +206,7 @@ MainWindow::MainWindow(const QString cfgfile, bool edit_conf, QWidget *parent) :
     connect(uiDockFft, SIGNAL(peakDetectionToggled(bool)), this, SLOT(setPeakDetection(bool)));
 
     // I/Q playback
-    connect(iq_tool, SIGNAL(startRecording()), this, SLOT(startIqRecording()));
+    connect(iq_tool, SIGNAL(startRecording(QString)), this, SLOT(startIqRecording(QString)));
     connect(iq_tool, SIGNAL(stopRecording()), this, SLOT(stopIqRecording()));
     connect(iq_tool, SIGNAL(startPlayback(QString,float)), this, SLOT(startIqPlayback(QString,float)));
     connect(iq_tool, SIGNAL(stopPlayback()), this, SLOT(stopIqPlayback()));
@@ -438,6 +438,7 @@ bool MainWindow::loadConfig(const QString cfgfile, bool check_crash)
     setNewFrequency(ui->freqCtrl->getFrequency()); // ensure all GUI and RF is updated
 
     remote->readSettings(m_settings);
+    iq_tool->readSettings(m_settings);
 
     return conf_ok;
 }
@@ -510,6 +511,7 @@ void MainWindow::storeSession()
         uiDockAudio->saveSettings(m_settings);
 
         remote->saveSettings(m_settings);
+        iq_tool->saveSettings(m_settings);
     }
 }
 
@@ -1275,7 +1277,7 @@ void MainWindow::stopAudioStreaming()
 }
 
 /*! \brief Start I/Q recording. */
-void MainWindow::startIqRecording()
+void MainWindow::startIqRecording(const QString recdir)
 {
     qDebug() << __func__;
     // generate file name using date, time, rf freq in kHz and BW in Hz
@@ -1283,7 +1285,7 @@ void MainWindow::startIqRecording()
     qint64 freq = ui->freqCtrl->getFrequency();
     qint64 sr = (qint64)(rx->get_input_rate());
     QString lastRec = QDateTime::currentDateTimeUtc().
-            toString("gqrx_yyyyMMdd_hhmmss_%1_%2_fc.'raw'").arg(freq).arg(sr);
+            toString("%1/gqrx_yyyyMMdd_hhmmss_%2_%3_fc.'raw'").arg(recdir).arg(freq).arg(sr);
 
     // start recorder; fails if recording already in progress
     if (rx->start_iq_recording(lastRec.toStdString()))
--- applications/gqrx/mainwindow.h
+++ applications/gqrx/mainwindow.h
@@ -151,7 +151,7 @@ private slots:
     void stopAudioStreaming();
 
     /* I/Q playback and recording*/
-    void startIqRecording();
+    void startIqRecording(const QString recdir);
     void stopIqRecording();
     void startIqPlayback(const QString filename, float samprate);
     void stopIqPlayback();
