21a8f35f328a4607e4c20418580a04d041b9260b
Alexandru Csete <oz9aec@gmail.com>
2016-01-29 21:31:28 +0100

    Dump unknown remote commands to console.

--- src/applications/gqrx/remote_control.cpp
+++ src/applications/gqrx/remote_control.cpp
@@ -23,6 +23,7 @@
 #include <cmath>
 #include <cstdlib>
 #include <iostream>
+#include <QDebug>
 #include <QString>
 #include <QStringList>
 #include "remote_control.h"
@@ -309,7 +310,9 @@ void RemoteControl::startRead()
 
     else
     {
-        // respond with an error
+        // print unknown command and respond with an error
+        qInfo() << "Unknown remote command:"
+                << cmdlist;
         rc_socket->write("RPRT 1\n");
     }
 }
