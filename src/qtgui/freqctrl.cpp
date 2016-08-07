5f7c0177f4d8084a1700284443f3bfd05cc92678
Alexandru Csete <oz9aec@gmail.com>
2016-08-07 17:42:55 +0200

    Reset frequency digits below the one being changed.

--- src/qtgui/freqctrl.cpp
+++ src/qtgui/freqctrl.cpp
@@ -77,7 +77,7 @@ CFreqCtrl::CFreqCtrl(QWidget *parent) :
     m_LastLeadZeroPos = 0;
     m_LRMouseFreqSel = false;
     m_ActiveEditDigit = -1;
-    m_ResetLowerDigits = false;
+    m_ResetLowerDigits = true;
     m_UnitsFont = QFont("Arial", 12, QFont::Normal);
     m_DigitFont = QFont("Arial", 12, QFont::Normal);
 
