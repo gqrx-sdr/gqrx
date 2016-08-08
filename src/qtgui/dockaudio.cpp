7120189177822ad8d5a07a91c46ca8f54cca6ad0
Alexandru Csete <oz9aec@gmail.com>
2016-08-08 11:14:17 +0200

    Set default audio FFT range to -70...0 dB.

--- src/qtgui/dockaudio.cpp
+++ src/qtgui/dockaudio.cpp
@@ -66,7 +66,7 @@ DockAudio::DockAudio(QWidget *parent) :
     ui->audioSpectrum->setFilterBoxEnabled(false);
     ui->audioSpectrum->setCenterLineEnabled(false);
     ui->audioSpectrum->setBookmarksEnabled(false);
-    ui->audioSpectrum->setMinMaxDB(-110, -20);
+    ui->audioSpectrum->setMinMaxDB(-70, 0);
     ui->audioSpectrum->setVdivDelta(20);
     ui->audioSpectrum->setHdivDelta(40);
     ui->audioSpectrum->setFreqDigits(1);
