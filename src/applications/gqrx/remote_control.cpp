gqrx-sdr/gqrx

commit 38f06530b58a6cdf7976cb13cdaba4d6f7c6309a

Add passband parameter to 'M/m' remote command
Fixes #218.

src/applications/gqrx/remote_control.cpp
@@ -268,19 +268,26 @@ void RemoteControl::startRead()
             }
             else
             {
-                rc_socket->write("RPRT 0\n");
                 rc_mode = mode;
+                emit newMode(rc_mode);
+
+                int passband = cmdlist.value(2, "0").toInt();
+                if ( passband != 0 )
+                    emit newPassband(passband);
 
                 if (rc_mode == 0)
                     audio_recorder_status = false;
 
-                emit newMode(rc_mode);
+                rc_socket->write("RPRT 0\n");
             }
         }
     }
     else if (cmdlist[0] == "m")
     {
-        rc_socket->write(QString("%1\n").arg(intToModeStr(rc_mode)).toLatin1());
+        QString msg = QString("%1 %2\n")
+                              .arg(intToModeStr(rc_mode))
+                              .arg(rc_passband_hi - rc_passband_lo);
+        rc_socket->write(msg.toLatin1());
     }
     else if (cmdlist[0] == "U")
     {
@@ -427,6 +434,13 @@ void RemoteControl::setMode(int mode)
 
     if (rc_mode == 0)
         audio_recorder_status = false;
+}
+
+/*! \brief Set passband (from mainwindow). */
+void RemoteControl::setPassband(int passband_lo, int passband_hi)
+{
+    rc_passband_lo = passband_lo;
+    rc_passband_hi = passband_hi;
 }
 
 /*! \brief New remote frequency received. */

src/applications/gqrx/mainwindow.cpp
@@ -267,6 +267,8 @@ MainWindow::MainWindow(const QString cfgfile, bool edit_conf, QWidget *parent) :
     connect(uiDockRxOpt, SIGNAL(sqlLevelChanged(double)), remote, SLOT(setSquelchLevel(double)));
     connect(remote, SIGNAL(startAudioRecorderEvent()), uiDockAudio, SLOT(startAudioRecorder()));
     connect(remote, SIGNAL(stopAudioRecorderEvent()), uiDockAudio, SLOT(stopAudioRecorder()));
+    connect(ui->plotter, SIGNAL(newFilterFreq(int, int)), remote, SLOT(setPassband(int, int)));
+    connect(remote, SIGNAL(newPassband(int)), this, SLOT(setPassband(int)));
 
     rds_timer = new QTimer(this);
     connect(rds_timer, SIGNAL(timeout()), this, SLOT(rdsTimeout()));
@@ -1069,6 +1071,8 @@ void MainWindow::selectDemod(int mode_idx)
     rx->set_filter((double)flo, (double)fhi, d_filter_shape);
     rx->set_cw_offset(cwofs);
     rx->set_sql_level(uiDockRxOpt->currentSquelchLevel());
+
+    remote->setPassband(flo, fhi);
 
     d_have_audio = (mode_idx != DockRxOpt::MODE_OFF);
 
@@ -2052,6 +2056,34 @@ void MainWindow::onBookmarkActivated(qint64 freq, QString demod, int bandwidth)
     {
         lo = hi - bandwidth;
     }
+
+    on_plotter_newFilterFreq(lo, hi);
+}
+
+void MainWindow::setPassband(int bandwidth)
+{
+    /* Check if filter is symmetric or not by checking the presets */
+    int mode = uiDockRxOpt->currentDemod();
+    int preset = uiDockRxOpt->currentFilterShape();
+
+    int lo, hi;
+    uiDockRxOpt->getFilterPreset(mode, preset, &lo, &hi);
+
+    if(lo + hi == 0)
+    {
+        lo = -bandwidth / 2;
+        hi =  bandwidth / 2;
+    }
+    else if(lo >= 0 && hi >= 0)
+    {
+        hi = lo + bandwidth;
+    }
+    else if(lo <= 0 && hi <= 0)
+    {
+        lo = hi - bandwidth;
+    }
+
+    remote->setPassband(lo, hi);
 
     on_plotter_newFilterFreq(lo, hi);
 }

src/applications/gqrx/mainwindow.h
@@ -155,6 +155,7 @@ private slots:
     void setSqlLevel(double level_db);
     double setSqlLevelAuto();
     void setAudioGain(float gain);
+    void setPassband(int bandwidth);
 
     /* audio recording and playback */
     void startAudioRec(const QString filename);

src/applications/gqrx/remote_control.h
@@ -85,6 +85,7 @@ public slots:
     void setBandwidth(qint64 bw);
     void setSignalLevel(float level);
     void setMode(int mode);
+    void setPassband(int passband_lo, int passband_hi);
     void setSquelchLevel(double level);
     void startAudioRecorder(QString unused);
     void stopAudioRecorder();
@@ -93,6 +94,7 @@ signals:
     void newFrequency(qint64 freq);
     void newFilterOffset(qint64 offset);
     void newMode(int mode);
+    void newPassband(int passband);
     void newSquelchLevel(double level);
     void startAudioRecorderEvent();
     void stopAudioRecorderEvent();
@@ -113,6 +115,8 @@ private:
     qint64      bw_half;
 
     int         rc_mode;           /*!< Current mode. */
+    int         rc_passband_lo;    /*!< Current low cutoff. */
+    int         rc_passband_hi;    /*!< Current high cutoff. */
     float       signal_level;      /*!< Signal level in dBFS */
     double      squelch_level;     /*!< Squelch level in dBFS */
     bool        audio_recorder_status; /*!< Recording enabled */

commit 25848239111426ddcb19a3087eaac99de11c612f

Reimplement a hamlib compatible layer.
The 'm' command must returns a passband on new line.

Fixes: 38f0653 (Add passband parameter to 'M/m' remote command.)
Fixes: 1b43d33 (Added a compatibility mode with hamlib specification
in remote control.)

src/applications/gqrx/remote_control.cpp
@@ -163,6 +163,8 @@ void RemoteControl::acceptConnection()
     }
     else
     {
+        /* new connction in non hamlib compatible mode */
+        hamlib_compatible = false;
         connect(rc_socket, SIGNAL(readyRead()), this, SLOT(startRead()));
     }
 }
@@ -257,7 +259,12 @@ void RemoteControl::startRead()
     {
         QString cmd_arg = cmdlist.value(1, "");
         if (cmd_arg == "?")
-            rc_socket->write("OFF RAW AM FM WFM WFM_ST WFM_ST_OIRT LSB USB CW CWL CWR CWU\n");
+        {
+            QString modes = (hamlib_compatible)
+                             ? "OFF RAW AM FM WFM WFM_ST WFM_ST_OIRT LSB USB CW CWR\n"
+                             : "OFF RAW AM FM WFM WFM_ST WFM_ST_OIRT LSB USB CWL CWU\n";
+            rc_socket->write(modes.toLatin1());
+        }
         else
         {
             int mode = modeStrToInt(cmd_arg);
@@ -284,9 +291,11 @@ void RemoteControl::startRead()
     }
     else if (cmdlist[0] == "m")
     {
-        QString msg = QString("%1 %2\n")
-                              .arg(intToModeStr(rc_mode))
-                              .arg(rc_passband_hi - rc_passband_lo);
+        QString msg = (hamlib_compatible)
+                       ? QString("%1\n%2\n")
+                         .arg(intToModeStr(rc_mode)).arg(rc_passband_hi - rc_passband_lo)
+                       : QString("%1 %2\n")
+                         .arg(intToModeStr(rc_mode)).arg(rc_passband_hi - rc_passband_lo);
         rc_socket->write(msg.toLatin1());
     }
     else if (cmdlist[0] == "U")
@@ -366,6 +375,7 @@ void RemoteControl::startRead()
      */
     else if (cmdlist[0] == "\\dump_state")
     {
+        hamlib_compatible = true;
         rc_socket->write("0\n"
                          "2\n"
                          "1\n"
@@ -543,22 +553,18 @@ int RemoteControl::modeStrToInt(QString mode_str)
     else if (mode_str.compare("CW", Qt::CaseInsensitive) == 0)
     {
         mode_int = 8;
-        hamlib_compatible = true;
     }
     else if (mode_str.compare("CWL", Qt::CaseInsensitive) == 0)
     {
         mode_int = 8;
-        hamlib_compatible = false;
     }
     else if (mode_str.compare("CWR", Qt::CaseInsensitive) == 0)
     {
         mode_int = 9;
-        hamlib_compatible = true;
     }
     else if (mode_str.compare("CWU", Qt::CaseInsensitive) == 0)
     {
         mode_int = 9;
-        hamlib_compatible = false;
     }
     else if (mode_str.compare("WFM_ST_OIRT", Qt::CaseInsensitive) == 0)
     {