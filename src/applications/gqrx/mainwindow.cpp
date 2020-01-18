2a0f99b46daa9c55c263faaa9a7f9e644c67427f
Clayton Smith <argilo@gmail.com>
2020-01-18 16:35:41 -0500

    Ensure d_fftAvg is initialized

--- src/applications/gqrx/mainwindow.cpp
+++ src/applications/gqrx/mainwindow.cpp
@@ -63,6 +63,7 @@ MainWindow::MainWindow(const QString cfgfile, bool edit_conf, QWidget *parent) :
     ui(new Ui::MainWindow),
     d_lnb_lo(0),
     d_hw_freq(0),
+    d_fftAvg(0.25),
     d_have_audio(true),
     dec_afsk1200(0)
 {
