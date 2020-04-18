20f40d7943636e6a83fd3e15d84ec7f1640e21ee
Tarmo Tanilsoo <tarmotanilsoo@gmail.com>
2020-04-18 15:16:05 +0300

    FM Stereo difference signal amplitude clarification in order to optimise stereo separation

--- src/dsp/stereo_demod.cpp
+++ src/dsp/stereo_demod.cpp
@@ -107,8 +107,8 @@ stereo_demod::stereo_demod(float input_rate, float audio_rate, bool stereo, bool
 
     mixer = gr::blocks::multiply_ff::make();
 
-    cdp = gr::blocks::multiply_const_ff::make( 3.6);
-    cdm = gr::blocks::multiply_const_ff::make(-3.6); 
+    cdp = gr::blocks::multiply_const_ff::make( 3.61); // ±0.02
+    cdm = gr::blocks::multiply_const_ff::make(-3.61); // ±0.02
 
     add0 = gr::blocks::add_ff::make();
     add1 = gr::blocks::add_ff::make();
