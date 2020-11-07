0dc3339b116b682d90f5314ec0656f0bc74e9a4c
Clayton Smith <argilo@gmail.com>
2020-11-07 01:28:14 -0500

    Set correct buffer size for audio FFT

--- src/applications/gqrx/receiver.cpp
+++ src/applications/gqrx/receiver.cpp
@@ -120,7 +120,7 @@ receiver::receiver(const std::string input_device,
     dc_corr = make_dc_corr_cc(d_decim_rate, 1.0);
     iq_fft = make_rx_fft_c(8192u, d_decim_rate, gr::filter::firdes::WIN_HANN);
 
-    audio_fft = make_rx_fft_f(8192u, gr::filter::firdes::WIN_HANN);
+    audio_fft = make_rx_fft_f(8192u, d_audio_rate, gr::filter::firdes::WIN_HANN);
     audio_gain0 = gr::blocks::multiply_const_ff::make(0);
     audio_gain1 = gr::blocks::multiply_const_ff::make(0);
     set_af_gain(DEFAULT_AUDIO_GAIN);
