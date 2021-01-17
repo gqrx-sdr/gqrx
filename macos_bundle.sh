c1e7f729747dc76957e9f9a5250708a4d71cba1f
Ulrich Loose <ulrich.loose@t-online.de>
2021-01-17 18:05:05 +0100

    rest2

--- macos_bundle.sh
+++ macos_bundle.sh
@@ -36,6 +36,15 @@ mkdir -p Gqrx.app/Contents/Resources
 </plist>
 EOM
 
+cp /Library/Frameworks/iio.framework/iio /usr/local/lib/libiio.dylib
+install_name_tool -id "/usr/local/lib/libiio.dylib" /usr/local/lib/libiio.dylib
+cp /Library/Frameworks/ad9361.framework/ad9361 /usr/local/lib/libad9361.dylib
+install_name_tool -id "/usr/local/lib/libad9361.dylib" /usr/local/lib/libad9361.dylib
+install_name_tool -delete_rpath /Library/Frameworks /usr/local/lib/libad9361.dylib
+install_name_tool -change @rpath/iio.framework/Versions/0.21/iio /usr/local/lib/libiio.dylib /usr/local/lib/libad9361.dylib
+install_name_tool -change @rpath/iio.framework/Versions/0.21/iio /usr/local/lib/libiio.dylib /usr/local/lib/SoapySDR/modules0.*/libPlutoSDRSupport.so
+install_name_tool -change @rpath/ad9361.framework/Versions/0.2/ad9361 /usr/local/lib/libad9361.dylib /usr/local/lib/SoapySDR/modules0.*/libPlutoSDRSupport.so
+
 cp build/src/gqrx Gqrx.app/Contents/MacOS
 cp resources/icons/gqrx.icns Gqrx.app/Contents/Resources
 cp -r /usr/local/lib/SoapySDR/modules* Gqrx.app/Contents/soapy-modules
