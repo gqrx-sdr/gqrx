6e8596ecc7ab7232506056db0e0a553b4c73c9ad
Vladisslav P <vladisslav@inbox.ru>
2022-01-01 04:20:37 +0300

    IQ tool: prevent accidental change of hw freq
    ...while playing back IQ file.
    
    Problem: It is possible to change the center frequency while playing an
    IQ file. In this case the real file center frequency does not get shifted
    to a correct position, so the spectrum plot/waterfall becomes shifted from
    actual played frequency, bookmarks become not valid, freqCtrl shows wrong
    frequency, plotter shows wrong frequency and so on.
    
    This commit changes frequency setting logic to be more staraightfroward
    and consistent.
    
    Independent of frequency change event source (freqctrls, plotter, remote)
    do the things in a same way: calculate new center and offset, taking into
    account the fact, that the center frequency may be loked due to IQ file
    playback, set the new frequency on a receiver side, then update all GUI
    controls to reflect changes.
    
    Enforce new frequency limits on the plotter side when IQ playback is
    started.

--- src/qtgui/freqctrl.cpp
+++ src/qtgui/freqctrl.cpp
@@ -51,7 +51,7 @@ CFreqCtrl::CFreqCtrl(QWidget *parent) :
 {
     setAutoFillBackground(false);
     setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-    setFocusPolicy(Qt::StrongFocus);
+    setFocusPolicy(Qt::WheelFocus);
     setMouseTracking(true);
     m_BkColor = QColor(0x1F, 0x1D, 0x1D, 0xFF);
     m_InactiveColor = QColor(0x43, 0x43, 0x43, 0xFF);
@@ -213,6 +213,13 @@ void CFreqCtrl::setup(int NumDigits, qint64 Minf, qint64 Maxf, int MinStep,
     m_NumSeps = (m_NumDigits - 1) / 3 - m_DigStart / 3;
 }
 
+void CFreqCtrl::localSetFrequency(qint64 freq)
+{
+    setFrequency(freq);
+    // signal the new frequency to world
+    emit    newFrequency(m_freq);
+}
+
 void CFreqCtrl::setFrequency(qint64 freq)
 {
     int       i;
@@ -272,12 +279,10 @@ void CFreqCtrl::setFrequency(qint64 freq)
                 m_DigitInfo[i].val = -m_DigitInfo[i].val;
         }
     }
-
-    // signal the new frequency to world
     m_Oldfreq = m_freq;
-    emit    newFrequency(m_freq);
     updateCtrl(m_LastLeadZeroPos != m_LeadZeroPos);
     m_LastLeadZeroPos = m_LeadZeroPos;
+
 }
 
 void CFreqCtrl::setDigitColor(QColor col)
@@ -535,7 +540,7 @@ void CFreqCtrl::keyPressEvent(QKeyEvent *event)
                 m_freq -= tmp * m_DigitInfo[m_ActiveEditDigit].weight;
                 m_freq = m_freq + (event->key() - '0') *
                          m_DigitInfo[m_ActiveEditDigit].weight;
-                setFrequency(m_freq);
+                localSetFrequency(m_freq);
             }
         }
         moveCursorRight();
@@ -731,7 +736,7 @@ void CFreqCtrl::incDigit()
                 }
                 m_freq = tmpl;
             }
-            setFrequency(m_freq);
+            localSetFrequency(m_freq);
         }
     }
 }
@@ -750,7 +755,7 @@ void CFreqCtrl::incFreq()
                 m_freq = m_freq - m_freq %
                          m_DigitInfo[m_ActiveEditDigit].weight;
             }
-            setFrequency(m_freq);
+            localSetFrequency(m_freq);
             m_LastEditDigit = m_ActiveEditDigit;
         }
     }
@@ -796,7 +801,7 @@ void CFreqCtrl::decDigit()
                 }
                 m_freq = tmpl;
             }
-            setFrequency(m_freq);
+            localSetFrequency(m_freq);
         }
     }
 }
@@ -816,7 +821,7 @@ void CFreqCtrl::decFreq()
                          m_DigitInfo[m_ActiveEditDigit].weight;
             }
 
-            setFrequency(m_freq);
+            localSetFrequency(m_freq);
             m_LastEditDigit = m_ActiveEditDigit;
         }
     }
@@ -835,7 +840,7 @@ void CFreqCtrl::clearFreq()
             /* digits below the active one are reset to 0 */
             m_freq -= m_freq % m_DigitInfo[m_ActiveEditDigit].weight;
 
-            setFrequency(m_freq);
+            localSetFrequency(m_freq);
             m_LastEditDigit = m_ActiveEditDigit;
         }
     }
