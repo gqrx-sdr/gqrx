fe056b832d33ce792aa5e3ee1827b857748764f6
Vladisslav P <vladisslav2011@gmail.com>
2022-08-11 23:47:42 +0300

    iq_formats: Add packed integer IQ formats
    10 bit: 2 samples packed into 5 bytes
    12 bit: 1 sample packed into 3 bytes
    14 bit: 2 samples packed into 7 bytes

--- src/dsp/format_converter.cpp
+++ src/dsp/format_converter.cpp
@@ -116,4 +116,130 @@ void any_to_any_impl::convert(const std::complex<uint8_t> *in, gr_complex * out,
     }
 }
 
+void any_to_any_impl::convert(const gr_complex *in, std::array<int8_t,5> * out, int noutput_items)
+{
+    while(noutput_items)
+    {
+        int i;
+        int q;
+        uint8_t * p = (uint8_t *) &(*out)[0];
+        i = std::round(in->real()*d_scale);
+        q = std::round(in->imag()*d_scale);
+        in++;
+        p[0] = i & 0xff;
+        p[1] = ((i & 0x0300) >> 8) | ((q & 0x3f) << 2);
+        p[2] = (q >> 6) & 0x0f;
+        i = std::round(in->real()*d_scale);
+        q = std::round(in->imag()*d_scale);
+        in++;
+        p[2] |= (i & 0x0f) << 4;
+        p[3] = ((i & 0x3f0) >> 4) | ((q & 0x03) << 6);
+        p[4] = q >> 2;
+        out++;
+        noutput_items--;
+    }
+}
+
+void any_to_any_impl::convert(const std::array<int8_t,5> *in, gr_complex * out, int noutput_items)
+{
+    noutput_items&=-2;
+    while(noutput_items)
+    {
+        int i;
+        int q;
+        uint8_t * p = (uint8_t *) &(*in)[0];
+        i = p[0] | ((p[1] & 0x03) << 8);
+        q = (p[1] >> 2) | ((p[2] & 0x0f) << 6);
+        *out=gr_complex(float((i&(1<<9))?i-1024:i)*d_scale_i, float((q&(1<<9))?q-1024:q)*d_scale_i);
+        out++;
+        i = (p[2] >> 4) | ((p[3] & 0x3f) << 4);
+        q = (p[3] >> 6) | (p[4] << 2);
+        *out=gr_complex(float((i&(1<<9))?i-1024:i)*d_scale_i, float((q&(1<<9))?q-1024:q)*d_scale_i);
+        out++;
+        in++;
+        noutput_items-=2;
+    }
+}
+
+void any_to_any_impl::convert(const gr_complex *in, std::array<int8_t,3> * out, int noutput_items)
+{
+    while(noutput_items)
+    {
+        int i;
+        int q;
+        uint8_t * p = (uint8_t *) &(*out)[0];
+        i = std::round(in->real()*d_scale);
+        q = std::round(in->imag()*d_scale);
+        in++;
+        p[0] = i & 0xff;
+        p[1] = (i & 0x0f00) >> 8 | (q & 0x0f)<<4;
+        p[2] = q >> 4;
+        out++;
+        noutput_items--;
+    }
+}
+
+void any_to_any_impl::convert(const std::array<int8_t,3> *in, gr_complex * out, int noutput_items)
+{
+    while(noutput_items)
+    {
+        int i;
+        int q;
+        uint8_t * p = (uint8_t *) &(*in)[0];
+        i = p[0] | (p[1] & 0x0f) << 8;
+        q = p[1] >> 4 | p[2] << 4;
+        *out=gr_complex(float((i&(1<<11))?i-4096:i)*d_scale_i, float((q&(1<<11))?q-4096:q)*d_scale_i);
+        out++;
+        in++;
+        noutput_items--;
+    }
+}
+
+void any_to_any_impl::convert(const std::array<int8_t,7> *in, gr_complex * out, int noutput_items)
+{
+    noutput_items&=-2;
+    while(noutput_items)
+    {
+        int i;
+        int q;
+        uint8_t * p = (uint8_t *) &(*in)[0];
+        i = p[0] | ((p[1] & 0x3f) << 8);
+        q = (p[1] >> 6) | (p[2] << 2) | ((p[3] & 0x0f) << 10);
+        *out=gr_complex(float((i&(1<<13))?i-16384:i)*d_scale_i, float((q&(1<<13))?q-16384:q)*d_scale_i);
+        out++;
+        i = (p[3] >> 4) | (p[4] << 4) | ((p[5] & 0x03) << 12);
+        q = (p[5] >> 2) | (p[6] << 6);
+        *out=gr_complex(float((i&(1<<13))?i-16384:i)*d_scale_i, float((q&(1<<13))?q-16384:q)*d_scale_i);
+        out++;
+        in++;
+        noutput_items-=2;
+    }
+}
+
+void any_to_any_impl::convert(const gr_complex *in, std::array<int8_t,7> * out, int noutput_items)
+{
+    while(noutput_items)
+    {
+        int i;
+        int q;
+        uint8_t * p = (uint8_t *) &(*out)[0];
+        i = std::round(in->real()*d_scale);
+        q = std::round(in->imag()*d_scale);
+        in++;
+        p[0] = i & 0xff;
+        p[1] = ((i & 0x3f00) >> 8) | ((q & 0x03) << 6);
+        p[2] = (q >> 2) & 0xff;
+        p[3] = (q >> 10) & 0x0f;
+        i = std::round(in->real()*d_scale);
+        q = std::round(in->imag()*d_scale);
+        in++;
+        p[3] |= (i & 0x0f) << 4;
+        p[4] = (i >> 4) & 0xff;
+        p[5] = ((i >> 12) & 0x03) | ((q & 0x3f) << 2);
+        p[6] = q >> 6;
+        out++;
+        noutput_items--;
+    }
+}
+
 constexpr std::array<any_to_any_base::format_descriptor, FILE_FORMAT_COUNT> any_to_any_base::fmt;
